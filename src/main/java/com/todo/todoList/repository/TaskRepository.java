c2fd5d4771b40703c5ab8d9f23ddb71c999329df
Itsumohitoride <luismiguelossaarias05@gmail.com>
2025-12-17 19:02:13 -0500

    feat(Repository): repository annotation has been added

--- src/main/java/com/todo/todoList/repository/TaskRepository.java
+++ src/main/java/com/todo/todoList/repository/TaskRepository.java
@@ -2,7 +2,9 @@ package com.todo.todoList.repository;
 
 import com.todo.todoList.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface TaskRepository extends JpaRepository<Task, UUID> { }
--- src/main/java/com/todo/todoList/repository/UserRepository.java
+++ src/main/java/com/todo/todoList/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.todo.todoList.repository;
 
 import com.todo.todoList.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, UUID> { }
